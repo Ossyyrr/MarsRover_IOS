d7f843e63836950d9fc35e6dc0e4e03880d9ae2d
Patricia Manzanero <patricia.manzanero-jimenez@capgemini.com>
2024-05-09 13:56:02 +0200

    command pattern

--- MarsRover.swift
+++ MarsRover.swift
@@ -23,8 +23,6 @@ class MarsRover {
         }
     }
 
-//    private var position: Position
-//    private var direction: RoverDirectionState
     private var status: RoverStatus
 
     private let mars = Mars()
@@ -32,31 +30,18 @@ class MarsRover {
     func execute(command: String) {
         for char in command {
             switch char {
-            case "f": status.position = moveForward()
-            case "b": status.position = moveBackward()
-            case "l": status.direction = status.direction.turnLeft()
-            case "r": status.direction = status.direction.turnRight()
+            case "f": status = MoveForwardCommand(roverStatus: status, mars: mars).execute()
+            case "b": status = MoveBackwardCommand(roverStatus: status, mars: mars).execute()
+            case "l": status = TurnLeftCommand(status: status).execute()
+            case "r": status = TurnRightCommand(status: status).execute()
             default: break
             }
         }
-//        print("Current position: \(position.x), \(position.y), \(direction)")
     }
 
     func getX() -> Int { status.position.x }
     func getY() -> Int { status.position.y }
     func getDirection() -> String { status.direction.toString() }
-
-    private func moveForward() -> Position {
-        var newPosition = status.position
-        switch status.direction {
-        case is NorthState: newPosition.y = (status.position.y == mars.limitY) ? 0 : (status.position.y + 1)
-        case is SouthState: newPosition.y = (status.position.y == 0) ? mars.limitY : (status.position.y - 1)
-        case is EastState: newPosition.x = (status.position.x == mars.limitX) ? 0 : (status.position.x + 1)
-        case is WestState: newPosition.x = (status.position.x == 0) ? mars.limitX : (status.position.x - 1)
-        default: break
-        }
-        return newPosition
-    }
 
     private func moveBackward() -> Position {
         var newPosition = status.position
--- MarsRover/RoverCommand.swift
+++ MarsRover/RoverCommand.swift
@@ -31,20 +31,48 @@ class MoveForwardCommand: RoverCommand {
     }
 }
 
-// class MoveBackwardCommand: RoverCommand {
-//    func execute() -> RoverStatus {
-//        <#code#>
-//    }
-// }
-//
-// class TurnLeftCommand: RoverCommand {
-//    func execute() -> RoverStatus {
-//        <#code#>
-//    }
-// }
-//
-// class TurnRightCommand: RoverCommand {
-//    func execute() -> RoverStatus {
-//        <#code#>
-//    }
-// }
+class MoveBackwardCommand: RoverCommand {
+    var status: RoverStatus
+    var mars: Mars
+
+    init(roverStatus: RoverStatus, mars: Mars) {
+        self.status = roverStatus
+        self.mars = mars
+    }
+
+    func execute() -> RoverStatus {
+        var newPosition = status.position
+        switch status.direction {
+        case is NorthState: newPosition.y = (status.position.y == 0) ? mars.limitY : (status.position.y - 1)
+        case is SouthState: newPosition.y = (status.position.y == mars.limitY) ? 0 : (status.position.y + 1)
+        case is EastState: newPosition.x = (status.position.x == 0) ? mars.limitX : (status.position.x - 1)
+        case is WestState: newPosition.x = (status.position.x == mars.limitX) ? 0 : (status.position.x + 1)
+        default: break
+        }
+        return RoverStatus(position: newPosition, direction: status.direction)
+    }
+}
+
+class TurnLeftCommand: RoverCommand {
+    var status: RoverStatus
+
+    init(status: RoverStatus) {
+        self.status = status
+    }
+
+    func execute() -> RoverStatus {
+        RoverStatus(position: status.position, direction: status.direction.turnLeft())
+    }
+}
+
+class TurnRightCommand: RoverCommand {
+    var status: RoverStatus
+
+    init(status: RoverStatus) {
+        self.status = status
+    }
+
+    func execute() -> RoverStatus {
+        RoverStatus(position: status.position, direction: status.direction.turnRight())
+    }
+}
