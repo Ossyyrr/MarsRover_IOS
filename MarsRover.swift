835a793066dc290d28419b70fe668719e88e3f55
Patricia Manzanero <patricia.manzanero-jimenez@capgemini.com>
2024-05-09 13:48:07 +0200

    wip

--- MarsRover.swift
+++ MarsRover.swift
@@ -13,57 +13,58 @@ class MarsRover {
         y: Int,
         direction: String
     ) {
-        self.position = Position(x: x, y: y)
+        self.status = RoverStatus(position: Position(x: x, y: y), direction: NorthState())
         switch direction {
-        case "N": self.direction = NorthState()
-        case "S": self.direction = SouthState()
-        case "E": self.direction = EastState()
-        case "W": self.direction = WestState()
-        default: self.direction = NorthState()
+        case "N": status.direction = NorthState()
+        case "S": status.direction = SouthState()
+        case "E": status.direction = EastState()
+        case "W": status.direction = WestState()
+        default: status.direction = NorthState()
         }
     }
 
-    private var position: Position
-    private var direction: RoverDirectionState
+//    private var position: Position
+//    private var direction: RoverDirectionState
+    private var status: RoverStatus
 
     private let mars = Mars()
 
     func execute(command: String) {
         for char in command {
             switch char {
-            case "f": position = moveForward()
-            case "b": position = moveBackward()
-            case "l": direction = direction.turnLeft()
-            case "r": direction = direction.turnRight()
+            case "f": status.position = moveForward()
+            case "b": status.position = moveBackward()
+            case "l": status.direction = status.direction.turnLeft()
+            case "r": status.direction = status.direction.turnRight()
             default: break
             }
         }
-        print("Current position: \(position.x), \(position.y), \(direction)")
+//        print("Current position: \(position.x), \(position.y), \(direction)")
     }
 
-    func getX() -> Int { position.x }
-    func getY() -> Int { position.y }
-    func getDirection() -> String { direction.toString() }
+    func getX() -> Int { status.position.x }
+    func getY() -> Int { status.position.y }
+    func getDirection() -> String { status.direction.toString() }
 
     private func moveForward() -> Position {
-        var newPosition = position
-        switch direction {
-        case is NorthState: newPosition.y = (position.y == mars.limitY) ? 0 : (position.y + 1)
-        case is SouthState: newPosition.y = (position.y == 0) ? mars.limitY : (position.y - 1)
-        case is EastState: newPosition.x = (position.x == mars.limitX) ? 0 : (position.x + 1)
-        case is WestState: newPosition.x = (position.x == 0) ? mars.limitX : (position.x - 1)
+        var newPosition = status.position
+        switch status.direction {
+        case is NorthState: newPosition.y = (status.position.y == mars.limitY) ? 0 : (status.position.y + 1)
+        case is SouthState: newPosition.y = (status.position.y == 0) ? mars.limitY : (status.position.y - 1)
+        case is EastState: newPosition.x = (status.position.x == mars.limitX) ? 0 : (status.position.x + 1)
+        case is WestState: newPosition.x = (status.position.x == 0) ? mars.limitX : (status.position.x - 1)
         default: break
         }
         return newPosition
     }
 
     private func moveBackward() -> Position {
-        var newPosition = position
-        switch direction {
-        case is NorthState: newPosition.y = (position.y == 0) ? mars.limitY : (position.y - 1)
-        case is SouthState: newPosition.y = (position.y == mars.limitY) ? 0 : (position.y + 1)
-        case is EastState: newPosition.x = (position.x == 0) ? mars.limitX : (position.x - 1)
-        case is WestState: newPosition.x = (position.x == mars.limitX) ? 0 : (position.x + 1)
+        var newPosition = status.position
+        switch status.direction {
+        case is NorthState: newPosition.y = (status.position.y == 0) ? mars.limitY : (status.position.y - 1)
+        case is SouthState: newPosition.y = (status.position.y == mars.limitY) ? 0 : (status.position.y + 1)
+        case is EastState: newPosition.x = (status.position.x == 0) ? mars.limitX : (status.position.x - 1)
+        case is WestState: newPosition.x = (status.position.x == mars.limitX) ? 0 : (status.position.x + 1)
         default: break
         }
         return newPosition
